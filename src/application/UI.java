a721b33ae23c13cfe4edb90178b246788397d7cf
Andre Kendji <andre.narita@gmail.com>
2019-11-28 01:37:43 -0300

    Placing pieces on the board and painting

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,31 +1,50 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
-	//User Interface
+	// User Interface
 	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
 	public static void printBoard(ChessPiece[][] pieces) {
+
+		for (int i = 0; i < pieces.length; i++) {
 		
-		for(int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
-			for(int j = 0; j < pieces.length; j++) {
+			
+			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
+		
 			System.out.println();
+			
 		}
+		
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	public static void printPiece(ChessPiece piece) {
-		if(piece == null) {
+		if (piece == null) {
 			System.out.print("-");
-		}
+		} 
+		
 		else {
-			System.out.print(piece);
+			
+			if(piece.getColor() == Color.WHITE) {
+				
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+			}
+			
+			else {
+				
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+			}
 		}
 		System.out.print(" ");
 	}
-	
 
 }
